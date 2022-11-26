1a89c928a8866e36fb5cff16a017d1ed0ee9df3b
jack-bots <75717012+vedamithra430@users.noreply.github.com>
2022-11-26 20:01:03 +0530

    Update rss.py

--- bot/modules/rss.py
+++ bot/modules/rss.py
@@ -293,8 +293,8 @@ async def rss_monitor():
                     url = rss_d.entries[feed_count]['links'][1]['href']
                 except IndexError:
                     url = rss_d.entries[feed_count]['link']
-                if RSS_COMMAND := config_dict['RSS_COMMAND']:
-                    feed_msg = f"{RSS_COMMAND} {url}"
+                if config_dict['RSS_COMMAND']:
+                    feed_msg = f"{config_dict['RSS_COMMAND']} {url}"
                 else:
                     feed_msg = f"<b>Name: </b><code>{rss_d.entries[feed_count]['title'].replace('>', '').replace('<', '')}</code>\n\n"
                     feed_msg += f"<b>Link: </b><code>{url}</code>"
